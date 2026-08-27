cb813ea18facf8659ef988c2cf6499745d615772
Jeevan <jeevank2782@gmail.com>
2026-08-27 23:29:46 +0530

    changes homepage

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
     return ( 
         <div className="container p-5">
             <div className='row text-center'>
-                <div className='heroHead mb-5 '>
+                <div className='heroHead mb-2 '>
                 <h1 className='hero-title mb-5'>Investing&nbsp;made&nbsp;easy</h1>
                 <button className='btn btn-primary btn-get'>Get started</button>
                 </div>
